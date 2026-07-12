2a6bdf030611ad41c5cbee6d1b3b6417486b9502
Tanush Singla <iamtanushsingla@gmail.com>
2026-07-12 22:03:54 +0530

    fix: explicitly add nitro vite plugin for vercel preset output generation

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     tanstackStart({
       server: { entry: "src/server.ts" },
+    }),
+    nitro({
+      preset: "vercel",
     }),
     tsconfigPaths(),
   ],
